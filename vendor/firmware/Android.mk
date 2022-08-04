ca3862a9fead1e3a1b32b6afbe12d9a774d9dfbb
Thomas Makin <halorocker89@gmail.com>
2022-08-03 23:43:14 -0400

    firmware: build hekate config files
    Change-Id: I01016e166c5dfc4b5f50a4d1a81b8f5f3b47a8ae

--- vendor/firmware/Android.mk
+++ vendor/firmware/Android.mk
@@ -51,3 +51,31 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
 include $(BUILD_PREBUILT)
 INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/coreboot.rom
+
+# Config
+include $(CLEAR_VARS)
+LOCAL_MODULE        := 00-android
+LOCAL_MODULE_SUFFIX := .ini
+LOCAL_SRC_FILES     := $(NX_VENDOR_PATH)/config/00-android.ini
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/00-android.ini
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := bootlogo_android
+LOCAL_MODULE_SUFFIX := .bmp
+LOCAL_SRC_FILES     := $(NX_VENDOR_PATH)/config/bootlogo_android.bmp
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/bootlogo_android.bmp
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := icon_android_hue
+LOCAL_MODULE_SUFFIX := .bmp
+LOCAL_SRC_FILES     := $(NX_VENDOR_PATH)/config/icon_android_hue.bmp
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/icon_android_hue.bmp
